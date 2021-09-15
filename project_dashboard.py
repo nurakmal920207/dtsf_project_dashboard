3160db7c0e45709d0d8656188a655d5d12546ee3
Akmal Nordi <85534549+nurakmal920207@users.noreply.github.com>
2021-09-15 18:23:26 +0800

    Update mean weighted days

--- project_dashboard.py
+++ project_dashboard.py
@@ -78,11 +78,13 @@ elif company_input == 'CIMA' and pwd_input == st.secrets[company_input]['pwd']: 
         df3['actual_day'] = df3.curr_progress * (df.end_date - df.start_date) / 100
         df3['expected_day'] = df.exp_progress * (df.end_date - df.start_date) / 100
         df3['days'] = df3.actual_day - df3.expected_day #calculate how many days ahead or late
-        if df3.days.mean().days < 0:
-            text = 'Overall Progress - %s days late' %(df3.days.mean().days*-1)
+        df3['weighted_days'] = df3.days * df.weightage
+        mean_weighted_days = df3.weighted_days.mean().days
+        if mean_weighted_days < 0:
+            text = 'Overall Progress - %s days late' %(mean_weighted_days*-1)
             color = "red"
-        elif df3.days.mean().days > 0:
-            text = 'Overall Progress - %s days ahead' %(df3.days.mean().days)
+        elif mean_weighted_days > 0:
+            text = 'Overall Progress - %s days ahead' %(mean_weighted_days)
             color = "green"
         else:
             text = 'Overall Progress - On time'
