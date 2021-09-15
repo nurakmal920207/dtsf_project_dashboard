e006903f1822bdfb0fad0fd5071791a58573a52a
Akmal Nordi <85534549+nurakmal920207@users.noreply.github.com>
2021-09-15 17:37:35 +0800

    Update project_dashboard.py

--- project_dashboard.py
+++ project_dashboard.py
@@ -118,7 +118,7 @@ elif company_input == 'CIMA' and pwd_input == st.secrets[company_input]['pwd']: 
                 plt.xlim([0,100])
                 ax.annotate('%s%%' %(str(df3.iloc[i,1])), (x_coord,-0.1), size = 20, color = color_)
                 if df3.iloc[i,-1].days > 0:
-                    ax.set_title('%s days ahead' %(str(df3.loc[i,'days'].days)), fontsize=12)
+                    ax.set_title('%s days ahead' %(str(df3.loc[i,'days'].days)), fontsize=18)
                 elif df3.iloc[i,-1].days < 0:
                     ax.set_title('%s days late' %(str(df3.loc[i,'days'].days*-1)))
                 else:
